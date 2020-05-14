19ecccd8887256424716026108ab32c501d8fd4d
nilsheinsohn <64216707+nilsheinsohn@users.noreply.github.com>
2020-05-14 23:38:43 +0200

    modified several programs for homework2

--- homework2/DAMI2_simpleRegression.py
+++ homework2/DAMI2_simpleRegression.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 from numpy import *
 import matplotlib.pyplot as plt 
@@ -8,37 +9,30 @@ data = genfromtxt('scores.csv', delimiter=',')
 x = array(data[:,0])
 y = array(data[:,1])
 
-#Plot the data for a first impression
-plt.figure(1)
-plt.scatter(x,y)
-plt.xlabel('Hours of study')
-plt.ylabel('Test scores')
-plt.show()
-
-
-
 def estimate_coef(x, y): 
 	# number of observations/points 
 	n = np.size(x) 
 
 	# HERE: Get the mean of x and y vector (hint: use built-in functions)
-	
-
+	m_x = np.mean(x) 
+	m_y = np.mean(y)
+    
 	# calculating cross-deviation and deviation about x 
 	SS_xy = np.sum(y*x) - n*m_y*m_x 
 	SS_xx = np.sum(x*x) - n*m_x*m_x 
 
 	# HERE: Calculate the regression coefficients 
-	beta_1 = 
-	beta_0 = 
+	beta_1 = np.sum((x-m_x)*(y-m_y)) / np.sum((x - m_x)**2)
+	beta_0 = m_y - beta_1*m_x
 	
 	return(beta_0, beta_1) 
 
 def plot_regression_line(x, y, b): 
 	# HERE: plot the actual points as scatter plot 
+	plt.scatter(x,y)
 
 	# HERE: compute the predicted response vector 
-	y_pred = 
+	y_pred = b[0] + b[1]*x
 
 	# plotting the regression line 
 	plt.plot(x, y_pred, color = "k") 
